29b28366d5c75a11d2f70b5d6727d5e9c31a7c4c
akshu1 <akshathanagaraj98@gmail.com>
2022-12-24 18:00:15 +0530

    Add:Program to create new contact by taking user input in AddressBook

--- main/java/com/bridgelabz/addressbook/AddressBookSystem.java
+++ main/java/com/bridgelabz/addressbook/AddressBookSystem.java
@@ -1,33 +1,35 @@
 package com.bridgelabz.addressbook;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBookSystem {
     public static void main(String[] args) {
+
         /*
          * Starting with displaying welcome to address program
          * */
 
         System.out.println("Welcome to Address Book Program");
-        Contacts.createContact("Akshatha","Anjananagar","Bangalore","Karnataka",
-                "560091","9110237051","akshatha98@gmail.com");
+        Contacts.createContact();
     }
+
+
     class Contacts {
-        /**creating contact using Arraylist **/
+        /**creating contact using Arraylist
+         * taking input from console
+         * **/
 
-        static void createContact(String name, String address, String city, String state, String zipCode, String phoneNo, String email) {
+        static void createContact() {
             ArrayList<String> firstContact = new ArrayList<String>(7);
-
-            firstContact.add(name);
-            firstContact.add(address);
-            firstContact.add(city);
-            firstContact.add(state);
-            firstContact.add(zipCode);
-            firstContact.add(phoneNo);
-            firstContact.add(email);
-
+            Scanner s = new Scanner(System.in);
+            System.out.println("Enter the contact details  : ");
+            System.out.println("Name Address City State Zipcode PhoneNo EmailId  : ");
+            for (int i = 0; i < 7; i++) {
+                String k = s.nextLine();
+                firstContact.add(k);
+            }
             System.out.println(firstContact );
-
         }
     }
 }
