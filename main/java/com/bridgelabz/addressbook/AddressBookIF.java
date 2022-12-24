383f0990c5bccbdf257ad3abd3db306c260fbf5b
akshu1 <akshathanagaraj98@gmail.com>
2022-12-24 20:09:45 +0530

    Add:Program to add multiple contacts by taking user input in AddressBook

---
+++ main/java/com/bridgelabz/addressbook/AddressBookIF.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressbook;
+
+public interface AddressBookIF {
+    void operation();
+    void addContact();
+    void deletePerson();
+    void displayContents();
+    void editPerson();
+
+}
