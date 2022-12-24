6557c907e7005bd0e033da5e129ee88b0c593a33
akshu1 <akshathanagaraj98@gmail.com>
2022-12-25 01:53:26 +0530

    Add:Program to search Contact by City or State in multiple AddressBook

--- main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,13 +1,12 @@
 package com.bridgelabz.addressbook;
 
-
-
 import java.util.Scanner;
 
 public class AddressBookMain {
     public static Scanner scannerObject = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("---------- Welcome To Address Book Program!! ----------");
+
         AddressBookDirectory addressBookDirectory = new AddressBookDirectory();
         addressBookDirectory.operationDirectory();
         AddressBook addressBook = new AddressBook();
