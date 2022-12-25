7cd17872665f70be2f8ade1590916eb87b3fff56
akshu1 <akshathanagaraj98@gmail.com>
2022-12-25 21:04:06 +0530

    Add:Program to view person by city or state  in AddressBook

--- main/java/com/bridgelabz/addressbook/AddressBookDirectoryIF.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.addressbook;
-
-public interface AddressBookDirectoryIF {
-    public  void addAddressBook();
-    public void operationDirectory();
-    public void displayDirectoryContents();
-    public void editAddressBook();
-}
